888aaf460b30bc8d9f6ba21f6c8bb140e94996cd
Sobihan Mahedran <sobihanmahedran@MacBook-Pro-de-Contentsquare.local>
2019-10-11 11:39:59 +0200

    [Added] A litle prototype... Need to fix a design :)

---
+++ json_to_chrome.py
@@ -0,0 +1,51 @@
+#!/usr/local/bin/python3
+import webbrowser
+from os import path
+from tkinter import * 
+# coding: utf-8
+
+
+def read_file(path):
+    file = open(path, "r").read()
+    return file.split("\n")
+
+def write_in_file(content, file):   
+    file.write(content)
+    file.write('\n')
+
+def open_url(content):
+    if (content == None)
+        exit()
+    url = []
+    split = content.split("\"")
+    print(split)
+    for i  in range(len(split)):
+        if split[i] == 'url_de_base':
+            url.append(split[i + 2])
+    if (len(url) <= 0):
+        print("[DEBUG] There is no url [DEBUG]")
+        exit()
+    chrome_path = "open -a /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome %s"
+    for i  in range(len(url)):
+        webbrowser.get(chrome_path).open(url[i])
+        print(url[i])
+
+my_string = None
+
+def get_entry():
+    global my_string, fenetre
+    my_string = entree.get()
+    fenetre.quit()
+
+fenetre = Tk()
+string = StringVar()
+label = Label(fenetre, text="Hello World")
+label.pack()
+value = StringVar() 
+value.set("texte par défaut")
+entree = Entry(fenetre, textvariable=string, width=30)
+entree.pack()
+bouton=Button(fenetre, text="Submit", command=get_entry)
+bouton.pack()
+fenetre.mainloop()
+open_url(str(my_string))
